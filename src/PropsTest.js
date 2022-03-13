39e7fb752a005f1be0f08e63f1007e7f471f671b
Ran Vijay Singh <ranvijaysingh403@gmail.com>
2022-03-13 09:17:07 +0530

    Learned useState,useEffect and applying styles in several way

--- src/PropsTest.js
+++ src/PropsTest.js
@@ -1,11 +1,19 @@
-function Propstest(props){
-    return(
-        <div>Hello {props.userName} testing click count: {props.clickCount}
-        <br/>
-        <div>Address: {props.address.city}, {props.address.state}</div>
+import { useEffect } from 'react';
+import './PropsTest.css';
+import style from './custom.module.css';
+
+function Propstest(props) {
+    useEffect(() => {
+        console.warn("______useEffect props_______");
+    });
+    return (
         <div>
-            <button onClick={props.testFunction}>Test props function call</button>
-        </div>
+            <div style={{ color: 'red', backgroundColor: 'yellow' }}>Hello {props.userName} testing click count: {props.clickCount} data total: {props.dataTotal}</div>
+            <br />
+            <div className='primary borderCol'>Address: {props.address.city}, {props.address.state}</div>
+            <div>
+                <button className={`${style.test} ${style.textCol}`} onClick={props.testFunction}>Test props function call</button>
+            </div>
         </div>
     )
 }
--- src/Retest.js
+++ src/Retest.js
@@ -1,15 +1,23 @@
-import {useState} from 'react';
+import {useState,useEffect} from 'react';
 import Propstest from './PropsTest';
 
 function Retest() {
     // let clickCount = "Ranvijay";
     const [clickCount,updateCount]=useState(0);
+    const [dataTotal,updateData]=useState(50);
+    useEffect(()=>{
+        console.warn("______useEffect data__________");
+    },[dataTotal]);
+    useEffect(()=>{
+        console.warn("______useEffect click");
+    },[clickCount]);
     return (
         <div>
-            <div>Hello React retesting. {clickCount}</div>
+            <div>Hello React retesting. Click count : {clickCount} and Data total : {dataTotal}</div>
             <button onClick={() => alert("Internal alert call")}>Internal click</button>
             <button onClick={functionCall}>Function call onclick</button>
-            <Propstest clickCount={clickCount} testFunction ={CallPropsFunction} userName={"Ranvijay"} address={{state:"UP",city:"Noida"}} />
+            <button onClick={()=>updateData(dataTotal+10)}>Test data</button>
+            <Propstest clickCount={clickCount} dataTotal={dataTotal} testFunction ={CallPropsFunction} userName={"Ranvijay"} address={{state:"UP",city:"Noida"}} />
         </div>
     );
     function functionCall() {
